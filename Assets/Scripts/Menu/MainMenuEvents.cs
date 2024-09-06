e5c24c5984a64e06d2056e35bf9597693dc8f81e
Nightwing31X <154690555+Nightwing31X@users.noreply.github.com>
2024-09-06 17:22:16 +0800

    Finished Options Menu, Play Menu and added mobile layout support
    Builds aren't added as they are to big. They are on the Itch page.

--- Assets/Scripts/Menu/MainMenuEvents.cs
+++ Assets/Scripts/Menu/MainMenuEvents.cs
@@ -1,18 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace MainMenu.UI
 {
     public class MainMenuEvents : MonoBehaviour
-    {
+    { 
+        [SerializeField] GameObject backMobileBTNs;
+        [SerializeField] GameObject backPCBTNs;
+        bool mobile;
+        bool PC;
+        string m_DeviceType;
+
         public void ExitToDesktop()
         {
             #if UNITY_EDITOR
-                    UnityEditor.EditorApplication.isPlaying = false;
-            #endif
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
             Application.Quit();
+        }
+        public void Openurl(string UrlName)
+        {
+            Application.OpenURL(UrlName);
+        }
+
+        void Awake()
+        {
+            backMobileBTNs.SetActive(false);
+            backPCBTNs.SetActive(false);
+        }
+
+        void Start()
+        {
+            //Check if the device running this is a desktop
+            if (SystemInfo.deviceType == DeviceType.Desktop)
+            {
+                m_DeviceType = "Desktop";
+                PC = true;
+                pcSettings();
+            }
+
+            //Check if the device running this is a handheld
+            if (SystemInfo.deviceType == DeviceType.Handheld)
+            {
+                m_DeviceType = "Handheld";
+                mobile = true;
+                mobileSettings();
+                //mobileBTNs.SetActive(true);
+            }
+            Debug.Log("Device type : " + m_DeviceType);
+        }
+
+        void mobileSettings()
+        {
+            if (mobile == true)
+            {
+                backPCBTNs.SetActive(!mobile);
+                backMobileBTNs.SetActive(mobile);
+            }
+        }
+
+        void pcSettings()
+        {
+            if (PC == true)
+            {
+                backPCBTNs.SetActive(PC);
+                backMobileBTNs.SetActive(!PC);
+            }
         }
     }
 }
